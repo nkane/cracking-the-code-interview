2263fae07e6972903104035bd1d987590709e1f0
Nick Kane <nkanedev@gmail.com>
2024-06-22 11:00:31 -0400

    chore: add palindrome answer

--- chapter-2-linked-list/questions/2.6/palindrome.go
+++ chapter-2-linked-list/questions/2.6/palindrome.go
@@ -8,5 +8,22 @@ import (
 	2.6: Palindrome: Implement a function to check if a linked list is a palindrome.
 */
 
-func Palindrome(l *linked_list.LinkedList[string]) {
+func Palindrome[T comparable](l *linked_list.LinkedList[T]) *linked_list.LinkedList[T] {
+	// create a new list an reverse it
+	newList := linked_list.LinkedList[T]{}
+	AddNode(&newList, l.Head)
+	return &newList
+}
+
+func AddNode[T comparable](l *linked_list.LinkedList[T], n *linked_list.Node[T]) *linked_list.Node[T] {
+	newNode := linked_list.Node[T]{
+		Data: n.Data,
+	}
+	if n.Next == nil {
+		l.Head = &newNode
+		return &newNode
+	}
+	addedNode := AddNode(l, n.Next)
+	addedNode.Next = &newNode
+	return &newNode
 }
--- chapter-2-linked-list/questions/2.6/palindrome_test.go
+++ chapter-2-linked-list/questions/2.6/palindrome_test.go
@@ -1,1 +1,69 @@
 package palindrome
+
+import (
+	"linked_list"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+type Test struct {
+	Nodes    []*linked_list.Node[string]
+	Expected string
+}
+
+func TestPalindrome(t *testing.T) {
+	tests := []Test{
+		{
+			Nodes: []*linked_list.Node[string]{
+				{
+					Data: "r",
+				},
+				{
+					Data: "a",
+				},
+				{
+					Data: "c",
+				},
+				{
+					Data: "e",
+				},
+				{
+					Data: "c",
+				},
+				{
+					Data: "a",
+				},
+				{
+					Data: "r",
+				},
+			},
+			Expected: `[data: r] -> [data: a] -> [data: c] -> [data: e] -> [data: c] -> [data: a] -> [data: r] -> `,
+		},
+
+		{
+			Nodes: []*linked_list.Node[string]{
+				{
+					Data: "r",
+				},
+				{
+					Data: "a",
+				},
+				{
+					Data: "t",
+				},
+			},
+			Expected: `[data: t] -> [data: a] -> [data: r] -> `,
+		},
+	}
+
+	for _, test := range tests {
+		l1 := &linked_list.LinkedList[string]{}
+		for _, node := range test.Nodes {
+			l1.Add(node)
+		}
+		newList := Palindrome(l1)
+		output := newList.StringSimple()
+		assert.Assert(t, output == test.Expected, "output does not match - expected: %s, got: %s", test.Expected, output)
+	}
+}

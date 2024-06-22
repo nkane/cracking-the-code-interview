21fe5467889fccb951f630804b9e401e1c032f51
Nick Kane <nkanedev@gmail.com>
2024-06-22 11:04:30 -0400

    chore: setting up 2.7 and 2.8 questions

---
+++ chapter-2-linked-list/questions/2.8/loop_detection.go
@@ -0,0 +1,10 @@
+package loop_detection
+
+/*
+	2.8: Loop Detection: Given a linked list which might contain a loop, implement an algorithm
+	that returns the node at the beginning of the loop (if one exists).
+
+	Example:
+	Input: a -> b -> c -> d -> e -> c [the same c as earlier]
+	Output: c
+*/

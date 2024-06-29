be261bce195de57bb4cbf02e4f01fa00a525bf62
Nick Kane <nkanedev@gmail.com>
2024-06-29 16:50:33 -0400

    update: added multistack as concept, code is buggy

--- chapter-3-stack-and-queues/solutions/3.1/three_in_one.go
+++ chapter-3-stack-and-queues/solutions/3.1/three_in_one.go
@@ -2,6 +2,7 @@ package three_in_one
 
 import (
 	"errors"
+	"log"
 )
 
 /*
@@ -25,6 +26,10 @@ Note: We will use the notation '[' to mean inclusive of an end opint and "(" to 
 - For stack 1, we will use [0, n/3).
 - For stack 2, we will use [n/3, 2n/3).
 - For stack 3, we will use [2n/3, n).
+
+If we had additional information about the expected usages of the stacks, then we could modify this algorithm
+accordingly. For example, if we expected Stack 1 to have many more elements than Stack 2, we could allocate
+more space to Stack 1 and less space to Stack 2.
 */
 
 type FixedMultiStack[T any] struct {
@@ -84,4 +89,176 @@ func (f *FixedMultiStack[T]) IndexOfTop(stackNum int) int {
 	offset := stackNum * f.StackCapacity
 	size := f.Sizes[stackNum]
 	return offset + size - 1
+}
+
+/*
+Approach 2: Flexible Divisions
+
+A second approach is to allow the stack blocks to be flexible in size. When one stack exceeds its initial
+capacity, we grow the allowable capacity and shift elements as necessary.
+
+We will also design our array to be circular, such that the final stack may start at the end of the array
+and wrap around to the beginning.
+
+Please not that the code for this solution is far more complex than would be appropriate for an interview.
+You could be responsible for pseudocode, or perhaps the code of individual components, but the entire
+implementatio would be far too much work.
+*/
+
+type StackInfo struct {
+	Start    int
+	Size     int
+	Capacity int
+	Max      int
+}
+
+func CreateStackInfo(start int, capacity int, max int) StackInfo {
+	return StackInfo{
+		Start:    start,
+		Capacity: capacity,
+		Size:     0,
+	}
+}
+
+// check if an index on the array is within the stack boundaries. The stack can wrap around to the start
+// of the array.
+func (si *StackInfo) IsWithintStackCapacity(index int) bool {
+	// if outside of bounds of array, return flase
+	if index < 0 || index >= si.Max {
+		return false
+	}
+	// if index wraps around, adjust it
+	contiguousIndex := index
+	if index < si.Start {
+		contiguousIndex = index + si.Max
+	}
+	end := si.Start + si.Capacity
+	return si.Start <= contiguousIndex && contiguousIndex < end
+}
+
+func (si *StackInfo) LastCapacityIndex() int {
+	return -1
+}
+
+func (si *StackInfo) LastElementIndex() int {
+	return -1
+}
+
+func (si *StackInfo) IsFull() bool {
+	return si.Size == si.Capacity
+}
+
+func (si *StackInfo) IsEmpty() bool {
+	return si.Size == 0
+}
+
+type MultiStack[T Zeroer[T]] struct {
+	StackInfo []StackInfo
+	Values    []T
+}
+
+type Zeroer[T any] interface {
+	Zero() T
+}
+
+func CreateMultiStack[T Zeroer[T]](numberOfStacks int, defaultSize int) *MultiStack[T] {
+	// create metadata for all the stack
+	max := numberOfStacks * defaultSize
+	info := make([]StackInfo, numberOfStacks)
+	for i := 0; i < numberOfStacks; i++ {
+		info[i] = CreateStackInfo(defaultSize*i, defaultSize, max)
+	}
+	return &MultiStack[T]{
+		StackInfo: info,
+		Values:    make([]T, max),
+	}
+}
+
+// push value onto stack num, shifting/expanding stacks as necessary.
+func (ms *MultiStack[T]) Push(stackNum int, value T) error {
+	if ms.AllStacksAreFull() {
+		return errors.New("all stacks are full")
+	}
+	stack := &ms.StackInfo[stackNum]
+	// if this stack is full, expand it
+	if stack.IsFull() {
+		ms.Expand(stackNum)
+	}
+	// find the index of the top element in the array + 1, and
+	// increment the stack pointer
+	stack.Size++
+	ms.Values[stack.LastElementIndex()] = value
+	return nil
+}
+
+func (ms *MultiStack[T]) Pop(stackNum int) (*T, error) {
+	stack := ms.StackInfo[stackNum]
+	if stack.IsEmpty() {
+		return nil, errors.New("stack is empty")
+	}
+	value := ms.Values[stack.LastElementIndex()]
+	ms.Values[stack.LastElementIndex()] = ms.Values[stack.LastElementIndex()].Zero()
+	stack.Size--
+	return &value, nil
+}
+
+func (ms *MultiStack[T]) Peek(stackNum int) *T {
+	stack := ms.StackInfo[stackNum]
+	return &ms.Values[stack.LastElementIndex()]
+}
+
+// returns the number of items actually present in the stack
+func (ms *MultiStack[T]) NumberOfElements() int {
+	size := 0
+	for _, si := range ms.StackInfo {
+		size += si.Size
+	}
+	return size
+}
+
+func (ms *MultiStack[T]) AllStacksAreFull() bool {
+	return ms.NumberOfElements() == len(ms.Values)
+}
+
+func (ms *MultiStack[T]) Expand(stackNum int) {
+	ms.Shift((stackNum + 1) % len(ms.StackInfo))
+	ms.StackInfo[stackNum].Capacity++
+}
+
+func (ms *MultiStack[T]) AdjustIndex(index int) int {
+	max := len(ms.Values)
+	return ((index % max) + max) % max
+}
+
+func (ms *MultiStack[T]) NextIndex(index int) int {
+	return ms.AdjustIndex(index + 1)
+}
+
+func (ms *MultiStack[T]) PreviousIndex(index int) int {
+	return ms.AdjustIndex(index - 1)
+}
+
+// shift items in stack over by one element, if we have available capacity, then
+// we'll end up shrinking the stack by one element, if we don't have available
+// capacity, then we'll need to shift the next stack over too.
+func (ms *MultiStack[Zeroer]) Shift(stackNum int) {
+	log.Printf("/// Shifting %d", stackNum)
+	stack := &ms.StackInfo[stackNum]
+	// if this stack is at its full capacity, then you need to move the next
+	// stack over by one element, this stack can now claim the freed index.
+	if stack.Size >= stack.Capacity {
+		nextStack := (stackNum + 1) % len(ms.StackInfo)
+		ms.Shift(nextStack)
+		stack.Capacity++ // claim index that next stack lost
+	}
+	// shift all elements in stack over by one
+	index := stack.LastCapacityIndex()
+	for stack.IsWithintStackCapacity(index) {
+		ms.Values[index] = ms.Values[ms.PreviousIndex(index)]
+		index = ms.PreviousIndex(index)
+	}
+	// adjust stack data
+	ms.Values[stack.Start] = ms.Values[stack.Start].Zero()
+	stack.Start = ms.NextIndex(stack.Start) // move start
+	stack.Capacity--                        // shrink capacity
 }
--- chapter-3-stack-and-queues/solutions/3.1/three_in_one_test.go
+++ chapter-3-stack-and-queues/solutions/3.1/three_in_one_test.go
@@ -6,7 +6,7 @@ import (
 	"gotest.tools/v3/assert"
 )
 
-func TestThreeInOne(t *testing.T) {
+func TestThreeInOne_Fixed(t *testing.T) {
 	fs := CreateFixedMultiStack[int](9)
 	fs.Push(0, 1)
 	fs.Push(0, 2)
@@ -22,6 +22,28 @@ func TestThreeInOne(t *testing.T) {
 
 	peekValue, err := fs.Peek(1)
 	assert.NilError(t, err)
+	assert.Assert(t, peekValue != nil)
+	assert.Assert(t, *peekValue != 1)
+}
+
+type IntZero int
+
+func (i IntZero) Zero() IntZero {
+	return 0
+}
+
+func TestThreeInOne_Multi(t *testing.T) {
+	ms := CreateMultiStack[IntZero](2, 10)
+
+	ms.Push(0, 1)
+	ms.Push(0, 2)
+	ms.Push(0, 3)
+
+	ms.Push(1, 4)
+	ms.Push(1, 5)
+	ms.Push(1, 6)
+
+	peekValue := ms.Peek(0)
 	assert.Assert(t, peekValue != nil)
 	assert.Assert(t, *peekValue != 1)
 }
